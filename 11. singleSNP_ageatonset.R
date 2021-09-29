6b7fac8e124e419163456dcd7d94fbddf0d2185e
CGEatTuebingen <91323698+CGEatTuebingen@users.noreply.github.com>
2021-09-29 16:28:07 +0200

    Update 11. singleSNP_ageatonset.R

--- 11. singleSNP_ageatonset.R
+++ 11. singleSNP_ageatonset.R
@@ -1,8 +1,9 @@
-# Individual SNP data 
+# Individual SNP data and its correlation with phenotype data 
 
-#BST1 SNP data extraction from plink
+#BST1 SNP data extraction (using plink on linux terminal); where mergedageatonset_pooled is the genotype data from PD GWAS on 8535 PD patients with AAO data
 plink --bfile mergedageatonset_pooled --snps 4:15737348:G:A --recode --out BSTA1SNP
 
+# On R platform
 bsta1_rs4698412 <- fread("BSTA1SNP.ped")
 bsta1_rs4698412$SNP <- paste(bsta1_rs4698412$V7, bsta1_rs4698412$V8, sep="")
 bsta1_rs4698412 <-  bsta1_rs4698412 %>% rename(FID=V1) %>% select(-V2,-V3, -V4, -V7, -V8, -V5, -V6)
