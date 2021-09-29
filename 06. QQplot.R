d4ce3c9a9a511d753e05277f67c1a42b52e6f6e2
CGEatTuebingen <91323698+CGEatTuebingen@users.noreply.github.com>
2021-09-29 14:42:28 +0200

    Update 06. QQplot.R

--- 06. QQplot.R
+++ 06. QQplot.R
@@ -5,6 +5,8 @@ library("GenABEL")
 
 # B. QQ plot
 thisqq <- qq(consoritum_metal_output$`P-value`)
+# Example of meta-analysis output file = "meta_analysis_courage_05052021.tbl" (See 04. Linear regression.R)
+
 ggsave(plot = thisqq, filename = "qq.png", units = c("in"), dpi = 600, height = 5.5, width = 5.5)
 
 # C. Estimation of lambda
