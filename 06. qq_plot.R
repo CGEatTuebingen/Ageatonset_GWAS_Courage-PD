af58217c91be3fa0a5b805132fb2fadb100fb0bd
CGEatTuebingen <91323698+CGEatTuebingen@users.noreply.github.com>
2021-09-30 10:29:37 +0200

    Update 06. qq_plot.R
    updates to naming of steps

--- 06. qq_plot.R
+++ 06. qq_plot.R
@@ -1,13 +1,13 @@
 # QQ plot generation and lambda value estimation
-# A. Load libraries
+## A. Load libraries
 library(qqman)
 library("GenABEL")
 
-# B. QQ plot
+## B. QQ plot
 thisqq <- qq(consoritum_metal_output$`P-value`)
 # Example of meta-analysis output file = "meta_analysis_courage_05052021.tbl" (See 04. Linear regression.R)
 
 ggsave(plot = thisqq, filename = "qq.png", units = c("in"), dpi = 600, height = 5.5, width = 5.5)
 
-# C. Estimation of lambda
+## C. Estimation of lambda
 estlambda(consoritum_metal_output$`P-value`, method="median")
